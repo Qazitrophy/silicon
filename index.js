0da5a2fe5cf6195b23a2dd7d73412f9322c9f5b1
Qazitrophy <64126525+Qazitrophy@users.noreply.github.com>
2020-07-23 01:03:28 +0600

    added html css file for silocon

---
+++ index.js
@@ -0,0 +1,12 @@
+//var amaunt = document.getElementById("validationCustom05");
+//var amaunt
+
+
+
+function amount () {
+   var document.getElementById("#validationCustom05").value;
+   var amount = validationCustom05;
+  
+}
+
+return amount; 
